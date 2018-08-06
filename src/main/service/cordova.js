747889df8b92858cc6085d1616d472abd5d21bcc
idnes.link <16096613+idnesdotlink@users.noreply.github.com>
2018-08-07 06:51:51 +0700

    cdv

--- src/main/service/cordova.js
+++ src/main/service/cordova.js
@@ -6,6 +6,14 @@ const CordovaService = {
       function () {
         console.log('device ready')
         store.commit('SETDEVICEREADY')
+        var notificationOpenedCallback = function (jsonData) {
+          console.log('notificationOpenedCallback: ' + JSON.stringify(jsonData))
+        }
+
+        window.plugins.OneSignal
+          .startInit('a5727d78-91ee-4b89-9296-b9f0e8e50545')
+          .handleNotificationOpened(notificationOpenedCallback)
+          .endInit()
       },
       false
     )
