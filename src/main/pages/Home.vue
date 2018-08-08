59fc06816010faf500eb0be7657c4531cca06857
itphcccoid <35017703+idcophccit@users.noreply.github.com>
2018-08-08 09:35:05 +0700

    scroll-fix

--- src/main/pages/Home.vue
+++ src/main/pages/Home.vue
@@ -5,26 +5,48 @@
       <transition :name="tabanimation">
         <router-view
           name="tab-content"
-          class="tab-content-scroll"/>
+          class="tab-content-scroll"
+          @scroll.native="onScroll($event)"/>
       </transition>
     </div>
   </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
+import { mapGetters, mapMutations } from 'vuex'
 export default {
   name: 'Home',
+  data () {
+    return {
+      scrollPosition: 0
+    }
+  },
   computed: {
     ...mapGetters([
       'tabanimation'
     ])
   },
   methods: {
+    ...mapMutations([
+      'SETTOOLBARHEIGHT'
+    ]),
     change (id) {
       this.$router.push({
         name: id
       })
+    },
+    onScroll ($event) {
+      let newPos = $event.target.scrollTop
+      let oldPos = this.scrollPosition
+      let ev = (oldPos > newPos) ? 'up' : 'down'
+      this.scrollPosition = newPos
+      console.log(ev)
+      /* let delta = oldPos - newPos
+      let absoluteDelta = Math.abs(delta)
+      this.position = newPos
+      this.SETTOOLBARHEIGHT(delta)
+      this.$emit(ev, { delta, absoluteDelta })
+      this.$emit('content-scroll', this.$el.scrollTop) */
     }
   }
 }
