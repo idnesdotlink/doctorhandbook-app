d111db91a245f33fe816a3bf0acb2d00a1620a4f
itphcccoid <35017703+idcophccit@users.noreply.github.com>
2018-08-10 14:27:13 +0700

    dexie

--- src/main/database/index.js
+++ src/main/database/index.js
@@ -2,24 +2,10 @@ import Dexie from 'dexie'
 import axios from 'axios'
 import DbStores from '@database/stores'
 import { forEach } from 'lodash'
+import { initStoragePersistence, showEstimatedQuota } from '@database/helper'
 const DbName = 'com.doctorhandbook.app'
 const DbVersion = 1
-/*
-import { initStoragePersistence, showEstimatedQuota } from '@database/helper'
 
-let _dexie
-
-const createDexie = async function () {
-  _dexie = new Dexie(DbName)
-  _dexie.version(DbVersion).stores(DbStores)
-  let db
-  try {
-    db = await _dexie.open()
-  } catch (error) {
-    console.log(error)
-  }
-  return db
-} */
 const install = function (Vue) {
   const _dexie = new Dexie(DbName)
   _dexie.version(DbVersion).stores(DbStores)
@@ -41,55 +27,15 @@ const install = function (Vue) {
   Vue.mixin({
     beforeCreate () {
       this.$_db = _dexie
+      this.$_db.helper = {
+        showEstimatedQuota,
+        initStoragePersistence
+      }
     }
   })
 }
 const database = {
   install
-  /* async install (Vue) {
-    let db = await createDexie()
-    db.on('ready', async function callback () {
-      let count = await db.drugs.count()
-      if (count > 0) return
-      let t
-      t = await axios.get('http://localhost:3000/drugs.json')
-      let x = []
-      let s = 1
-      forEach(t.data.mims, async i => {
-        x.push({
-          id: s++,
-          title: i.name
-        })
-      })
-      console.log(x)
-      await db.drugs.bulkAdd(x)
-    })
-
-    Vue.mixin({
-      beforeCreate () {
-        this.$db = {
-          db
-        }
-      }
-    })
-
-    Vue.prototype.$db = {
-      db,
-      clearDB () {
-        try {
-          window.indexedDB.deleteDatabase('__dbnames')
-          window.indexedDB.deleteDatabase(DbName)
-          db.close()
-        } catch (error) {
-          console.log(error)
-        }
-      },
-      initStoragePersistence,
-      showEstimatedQuota,
-      info () {
-      }
-    }
-  } */
 }
 
 export default database
